9f5c928f4d075191b867565ccd1af519bf87f7e3
Clement Calmels <clement.calmels@free.fr>
2024-12-03 23:03:54 +0100

    do day03 in one regex

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -6,8 +6,7 @@ fn resolve<T>(lines: Lines<T>) -> (u32, u32)
 where
     T: BufRead,
 {
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let re2 = Regex::new(r"do\(\)|don\'t\(\)|mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let re = Regex::new(r"do\(\)|don\'t\(\)|mul\((\d{1,3}),(\d{1,3})\)").unwrap();
     let mut part1 = 0;
     let mut part2 = 0;
     let mut multiply = true;
@@ -15,27 +14,22 @@ where
     for line in lines {
 	let line = line.unwrap();
 
-	part1 = re.captures_iter(&line).fold(part1, |acc, c| {
-	    let x = u32::from_str(c.get(1).map_or("", |m| m.as_str())).unwrap();
-	    let y = u32::from_str(c.get(2).map_or("", |m| m.as_str())).unwrap();
-
-	    acc + x * y
-	});
-
-	part2 = re2.captures_iter(&line).fold(part2, |acc, c| {
+	(part1, part2) = re.captures_iter(&line).fold((part1, part2), |(p1, p2), c| {
 	    match c.get(0).unwrap().as_str() {
 		"do()" => multiply = true,
 		"don't()" => multiply = false,
 		_ => {
-		    if multiply {
-			let x = u32::from_str(c.get(1).map_or("", |m| m.as_str())).unwrap();
-			let y = u32::from_str(c.get(2).map_or("", |m| m.as_str())).unwrap();
+		    let x = u32::from_str(c.get(1).map_or("", |m| m.as_str())).unwrap();
+		    let y = u32::from_str(c.get(2).map_or("", |m| m.as_str())).unwrap();
 
-			return acc + x * y;
+		    if multiply {
+			return (p1 + x * y, p2 + x * y);
+		    } else {
+			return (p1 + x * y, p2);
 		    }
 		}
 	    }
-	    acc
+	    (p1, p2)
 	});
     }
 
