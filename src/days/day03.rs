a8e6317fc1d790f5479195abdb61f3bb37dbd8d7
Clement Calmels <clement.calmels@qorvo.com>
2024-12-25 21:39:48 +0100

    modify day03

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,39 +1,27 @@
 use regex::Regex;
 use std::io::{BufRead, Lines};
-use std::str::FromStr;
 
-fn resolve<T>(lines: Lines<T>) -> (u32, u32)
+fn resolve<T>(lines: Lines<T>) -> (u32, u32, bool)
 where
     T: BufRead,
 {
     let re = Regex::new(r"do\(\)|don\'t\(\)|mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let mut part1 = 0;
-    let mut part2 = 0;
-    let mut multiply = true;
 
-    for line in lines {
-        let line = line.unwrap();
+    lines.fold((0, 0, true), |(part1, part2, multiply), line| {
+        re.captures_iter(&line.unwrap())
+            .fold((part1, part2, multiply), |(p1, p2, m), c| {
+                match c.get(0).unwrap().as_str() {
+                    "do()" => (p1, p2, true),
+                    "don't()" => (p1, p2, false),
+                    _ => {
+                        let v = c.get(1).map_or("", |m| m.as_str()).parse::<u32>().unwrap()
+                            * c.get(2).map_or("", |m| m.as_str()).parse::<u32>().unwrap();
 
-        (part1, part2) = re.captures_iter(&line).fold((part1, part2), |(p1, p2), c| {
-            match c.get(0).unwrap().as_str() {
-                "do()" => multiply = true,
-                "don't()" => multiply = false,
-                _ => {
-                    let x = u32::from_str(c.get(1).map_or("", |m| m.as_str())).unwrap();
-                    let y = u32::from_str(c.get(2).map_or("", |m| m.as_str())).unwrap();
-
-                    if multiply {
-                        return (p1 + x * y, p2 + x * y);
-                    } else {
-                        return (p1 + x * y, p2);
+                        (p1 + v, if m { p2 + v } else { p2 }, m)
                     }
                 }
-            }
-            (p1, p2)
-        });
-    }
-
-    (part1, part2)
+            })
+    })
 }
 
 #[test]
@@ -41,7 +29,7 @@ fn check() {
     const TEST: &str = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
     use std::io::Cursor;
 
-    assert_eq!(resolve(Cursor::new(TEST).lines()), (161, 48));
+    assert_eq!(resolve(Cursor::new(TEST).lines()), (161, 48, true));
 }
 
 fn resolve_string<T>(lines: Lines<T>) -> (String, String)
