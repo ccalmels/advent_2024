1d2437a7aa8bf80929764d2c68422b972df7072c
Clement Calmels <clement.calmels@free.fr>
2026-06-29 12:14:50 +0200

    use iterator for day17

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -1,204 +1,195 @@
 use regex::Regex;
-use std::fmt::Write;
+use std::fmt::{Display, Formatter, Result, Write};
 use std::io::{BufRead, Lines};
 
-#[derive(Debug, Clone)]
-struct Computer {
-    registers: [u64; 3],
-    program: Vec<u8>,
+struct Cpu {
+    a: u64,
+    b: u64,
+    c: u64,
     pc: usize,
-    out: Vec<u8>,
 }
 
-impl Computer {
-    fn new(program: Vec<u8>) -> Self {
-        let registers = [0; 3];
-        let pc = 0;
-        let out = vec![];
-
-        Computer {
-            registers,
-            program,
-            pc,
-            out,
+impl Cpu {
+    fn new(registers: [u64; 3]) -> Self {
+        Cpu {
+            a: registers[0],
+            b: registers[1],
+            c: registers[2],
+            pc: 0,
         }
     }
 
     fn combo(&self, value: u8) -> u64 {
         match value {
             0..4 => value as u64,
-            4..7 => self.registers[(value - 4) as usize],
+            4 => self.a,
+            5 => self.b,
+            6 => self.c,
             _ => panic!(),
         }
     }
 
     fn adv(&mut self, operand: u8) {
-        //println!("adv {operand}");
-        self.registers[0] >>= self.combo(operand);
+        self.a >>= self.combo(operand);
     }
 
     fn bxl(&mut self, operand: u8) {
-        //println!("bxl {operand}");
-        self.registers[1] ^= operand as u64;
+        self.b ^= operand as u64;
     }
 
     fn bst(&mut self, operand: u8) {
-        //println!("bst {operand}");
-        self.registers[1] = self.combo(operand) & 0x7;
+        self.b = self.combo(operand) & 0x7;
     }
 
-    fn jnz(&mut self, operand: u8) -> bool {
-        //println!("jnz {operand}");
-        if self.registers[0] == 0 {
-            false
-        } else {
+    fn jnz(&mut self, operand: u8) {
+        if self.a != 0 {
             self.pc = operand as usize;
-            true
         }
-        //println!();
     }
 
-    fn bxc(&mut self, _operand: u8) {
-        //println!("bxc");
-        self.registers[1] ^= self.registers[2];
+    fn bxc(&mut self) {
+        self.b ^= self.c;
     }
 
-    fn out(&mut self, operand: u8) {
-        //println!("out {operand}");
-        //println!();
-        self.out.push((self.combo(operand) & 0x7) as u8);
+    fn out(&mut self, operand: u8) -> u8 {
+        (self.combo(operand) & 0x7) as u8
     }
 
     fn bdv(&mut self, operand: u8) {
-        //println!("bdv {operand}");
-        self.registers[1] = self.registers[0] >> self.combo(operand);
+        self.b = self.a >> self.combo(operand);
     }
 
     fn cdv(&mut self, operand: u8) {
-        //println!("cdv {operand}");
-        self.registers[2] = self.registers[0] >> self.combo(operand);
+        self.c = self.a >> self.combo(operand);
     }
+}
 
-    fn run(&mut self, registers: [u64; 3]) {
-        self.registers = registers;
-        self.pc = 0;
-        self.out.clear();
+impl Display for Cpu {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "A: {}, B: {}, C: {}, PC: {}",
+            self.a, self.b, self.c, self.pc
+        )
+    }
+}
 
-        while self.pc < self.program.len() {
-            let opcode = self.program[self.pc];
-            let operand = self.program[self.pc + 1];
+struct Program(Vec<u8>);
+
+struct ProgramIterator<'a> {
+    program: &'a Program,
+    cpu: Cpu,
+}
+
+impl<'a> Iterator for ProgramIterator<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.cpu.pc < self.program.0.len() {
+            let opcode = self.program.0[self.cpu.pc];
+            let operand = self.program.0[self.cpu.pc + 1];
+
+            self.cpu.pc += 2;
 
             match opcode {
-                0 => self.adv(operand),
-                1 => self.bxl(operand),
-                2 => self.bst(operand),
-                3 => {
-                    if self.jnz(operand) {
-                        continue;
-                    }
-                }
-                4 => self.bxc(operand),
-                5 => self.out(operand),
-                6 => self.bdv(operand),
-                7 => self.cdv(operand),
+                0 => self.cpu.adv(operand),
+                1 => self.cpu.bxl(operand),
+                2 => self.cpu.bst(operand),
+                3 => self.cpu.jnz(operand),
+                4 => self.cpu.bxc(),
+                5 => return Some(self.cpu.out(operand)),
+                6 => self.cpu.bdv(operand),
+                7 => self.cpu.cdv(operand),
                 _ => panic!(),
             }
-
-            //println!("A: {:#050b}", self.registers[0]);
-            //println!("B: {:#050b}", self.registers[1]);
-            //println!("C: {:#050b}", self.registers[2]);
-
-            self.pc += 2;
-        }
-    }
-
-    fn out_string(&self) -> String {
-        self.out.iter().fold(String::new(), |mut s, &n| {
-            if s.is_empty() {
-                write!(s, "{}", n).ok();
-            } else {
-                write!(s, ",{}", n).ok();
-            }
-            s
-        })
-    }
-
-    fn match_the_end(&self) -> bool {
-        self.out
-            .iter()
-            .rev()
-            .zip(self.program.iter().rev())
-            .all(|(a, b)| a == b)
-    }
-
-    fn get_register_a_values(&mut self, n: usize) -> Vec<u64> {
-        if n == 0 {
-            return vec![0];
         }
 
-        self.get_register_a_values(n - 1)
-            .iter()
-            .flat_map(|&next| (0..8).map(move |i| (next, i)))
-            .filter_map(|(next, i)| {
-                let a = (next << 3) + i;
-
-                self.run([a, 0, 0]);
-
-                let is_ok = self.out.len() == n && self.match_the_end();
-
-                if is_ok {
-                    Some(a)
-                } else {
-                    None
-                }
-            })
-            .collect()
+        None
     }
+}
 
-    fn get_register_a_value(&mut self) -> u64 {
-        let n = self.program.len();
+impl<'a> Display for ProgramIterator<'a> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        self.cpu.fmt(f)
+    }
+}
 
-        assert!(n > 0);
-
-        for next in self.get_register_a_values(n - 1) {
-            for i in 0..8 {
-                let a = (next << 3) + i;
-
-                self.run([a, 0, 0]);
-
-                let is_ok = self.out.len() == n && self.match_the_end();
-
-                if is_ok {
-                    return a;
-                }
-            }
+impl Program {
+    fn run(&self, registers: [u64; 3]) -> ProgramIterator<'_> {
+        ProgramIterator {
+            program: self,
+            cpu: Cpu::new(registers),
         }
-
-        0
     }
 }
 
 #[test]
-fn check_match_the_end() {
-    let mut c = Computer::new(vec![0, 1, 2, 3, 4]);
+fn check_program() {
+    let program = Program(vec![0, 1, 5, 4, 3, 0]);
+    let output = vec![4, 6, 3, 5, 6, 3, 5, 2, 1, 0];
 
-    c.out = vec![4];
-    assert!(c.match_the_end());
+    for (a, &b) in program.run([729, 0, 0]).zip(output.iter()) {
+        assert_eq!(a, b);
+    }
 
-    c.out = vec![3];
-    assert!(!c.match_the_end());
+    let program = Program(vec![0, 3, 5, 4, 3, 0]);
 
-    c.out = vec![3, 4];
-    assert!(c.match_the_end());
+    for (a, &b) in program.run([117440, 0, 0]).zip(program.0.iter()) {
+        assert_eq!(a, b);
+    }
+}
 
-    c.out = vec![2, 4];
-    assert!(!c.match_the_end());
+fn check_first_output(program: &Program, a: u64, expected: u8) -> Option<u64> {
+    let output = program.run([a, 0, 0]).next().unwrap();
 
-    c.out = vec![0, 1, 2, 3, 4];
-    assert!(c.match_the_end());
+    if output == expected {
+        Some(a)
+    } else {
+        None
+    }
+}
 
-    c.out = vec![0, 1, 2, 3, 4, 5];
-    assert!(!c.match_the_end());
+fn find_values(n: usize, program: &Program) -> Vec<u64> {
+    assert!(n > 0);
+    assert!(n <= program.0.len());
+
+    let expected = program.0[program.0.len() - n];
+
+    if n == 1 {
+        (1..8)
+            .filter_map(|a| check_first_output(program, a, expected))
+            .collect()
+    } else {
+        find_values(n - 1, program)
+            .iter()
+            .flat_map(|v| (0..8).map(move |i| (v << 3) + i))
+            .filter_map(|a| check_first_output(program, a, expected))
+            .collect()
+    }
+}
+
+fn get_a(program: &Program) -> u64 {
+    let vs = find_values(program.0.len(), program);
+
+    if vs.is_empty() {
+        u64::MAX
+    } else {
+        vs[0]
+    }
+}
+
+fn get_output(program: &Program, registers: [u64; 3]) -> String {
+    let mut output = String::new();
+
+    for o in program.run(registers) {
+        if output.is_empty() {
+            write!(output, "{o}").unwrap();
+        } else {
+            write!(output, ",{o}").unwrap();
+        }
+    }
+
+    output
 }
 
 fn resolve<T>(lines: Lines<T>) -> (String, u64)
@@ -232,11 +223,9 @@ where
         }
     }
 
-    let mut computer = Computer::new(program);
+    let program = Program(program);
 
-    computer.run(registers);
-
-    (computer.out_string(), computer.get_register_a_value())
+    (get_output(&program, registers), get_a(&program))
 }
 
 #[test]
